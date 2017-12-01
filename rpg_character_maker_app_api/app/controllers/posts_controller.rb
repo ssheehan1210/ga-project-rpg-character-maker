024a1902e797719a2ed45738ef3fdb01a0040d19
Sam Sheehan <ssheehan1210@yahoo.com>
2017-12-01 16:45:09 -0600

    Prepared a post controller and set up a database seed that will send my DaD character stats in the database

---
+++ rpg_character_maker_app_api/app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def post_params
+	end
+
+end
